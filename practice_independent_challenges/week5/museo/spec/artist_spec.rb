86e377ad3a6c9b6e4ea903b52594c0f9a061497a
Crow Rising <miscbyrd@gmail.com>
2023-02-25 19:09:06 -0700

    Create artist tests

---
+++ practice_independent_challenges/week5/museo/spec/artist_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+RSpec.describe do
+  before(:each) do 
+    @artist = Artist.new
+    ({
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    })
+  end
+  describe '#initialize artist' do
+    it 'exists' do
+      expect(@artist).to be_a(Artist)
+    end
+
+    it 'has attributes' do
+      expect(@artist.id).to eq(2)
+      expect(@artist.name).to eq("Ansel Adams")
+      expect(@artist.born).to eq(1902)
+      expect(@artist.died).to eq(1984)
+      expect(@artist.country).to eq("United States")
+      expect(@artist.age_at_death).to eq(82)
+    end
+  end
+end
--- practice_independent_challenges/week5/museo/spec/photograph_spec.rb
+++ practice_independent_challenges/week5/museo/spec/photograph_spec.rb
@@ -9,15 +9,8 @@ RSpec.describe do
       artist_id: "4",
       year: "1954"
     })
-    @artist = Artist.new
-    ({
-      id: "2",
-      name: "Ansel Adams",
-      born: "1902",
-      died: "1984",
-      country: "United States"
-    })
   end
+
   describe '#intialize photo' do
     it 'exists' do
       expect(@photograph).to be_a(Photograph)
@@ -30,20 +23,4 @@ RSpec.describe do
       expect(@photograph.artist_id).to eq(4)
     end
   end
-
-  describe '#initialize artist' do
-    it 'exists' do
-      expect(@artist).to be_a(Artist)
-    end
-
-    it 'has attributes' do
-      expect(@artist.id).to eq(2)
-      expect(@artist.name).to eq("Ansel Adams")
-      expect(@artist.born).to eq(1902)
-      expect(@artist.died).to eq(1984)
-      expect(@artist.country).to eq("United States")
-      expect(@artist.age_at_death).to eq(82)
-    end
-  end
-
 end
