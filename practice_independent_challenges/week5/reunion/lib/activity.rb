2db1928c8817011491a3bc71b93691eaa489c936
Crow Rising <miscbyrd@gmail.com>
2023-02-26 15:00:46 -0700

    Complete activity class

--- practice_independent_challenges/week5/reunion/lib/activity.rb
+++ practice_independent_challenges/week5/reunion/lib/activity.rb
@@ -1,7 +1,18 @@
 class Activity
-  attr_reader :name
+  attr_reader :name,
+              :participants
               
   def initialize(name)
     @name = name
+    @participants = {}
+  end
+
+  def add_participant(name, cost)
+    @participants[name] = cost
+    return @participants
+  end
+
+  def total_cost
+   total_cost = @participants.values.sum
   end
 end
