91b876e4becae40437d091280b3b74fe6095d669
Crow Rising <miscbyrd@gmail.com>
2023-02-25 19:03:10 -0700

    Create test to initialize artist

--- practice_independent_challenges/week5/museo/spec/photograph_spec.rb
+++ practice_independent_challenges/week5/museo/spec/photograph_spec.rb
@@ -2,18 +2,38 @@ require 'spec_helper'
 
 RSpec.describe do
   before(:each) do 
-    @photograph = Photograph.new(attributes)
-    @artist = Artist.new(attributes)
+    @photograph = Photograph.new
+    ({
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "4",
+      year: "1954"
+    })
+    @artist = Artist.new
+    ({
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    })
   end
-  describe '#intialize' do
+  describe '#intialize photo' do
     it 'exists' do
       expect(@photograph).to be_a(Photograph)
     end
 
     it 'has attributes' do
-      expect(@photograph.id)
-      expect(@photograph.artist_id)
-      expect(@photograph.year)
+      expect(@photograph.id).to eq(1)
+      expect(@photograph.artist_id).to eq("Rue Mouffetard, Paris (Boy with Bottles)")
+      expect(@photograph.year).to eq(1954)
+      expect(@photograph.artist_id).to eq(4)
+    end
+  end
+
+  describe '#initialize artist' do
+    it 'exists' do
+      expect(@artist).to be_a(Artist)
     end
   end
 
