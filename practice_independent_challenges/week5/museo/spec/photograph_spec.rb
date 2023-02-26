cb08d2610d70faeea2c1e45d4ad12723b2da35d8
Crow Rising <miscbyrd@gmail.com>
2023-02-25 19:07:07 -0700

    Finish tests for iteration 1

--- practice_independent_challenges/week5/museo/spec/photograph_spec.rb
+++ practice_independent_challenges/week5/museo/spec/photograph_spec.rb
@@ -35,6 +35,15 @@ RSpec.describe do
     it 'exists' do
       expect(@artist).to be_a(Artist)
     end
+
+    it 'has attributes' do
+      expect(@artist.id).to eq(2)
+      expect(@artist.name).to eq("Ansel Adams")
+      expect(@artist.born).to eq(1902)
+      expect(@artist.died).to eq(1984)
+      expect(@artist.country).to eq("United States")
+      expect(@artist.age_at_death).to eq(82)
+    end
   end
 
 end
