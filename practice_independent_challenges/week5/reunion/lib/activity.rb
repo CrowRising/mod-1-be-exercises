4250e19a46e0013302e313e95ad50bbabd4e4f42
Crow Rising <miscbyrd@gmail.com>
2023-02-26 20:18:42 -0700

    Add tests to activity for iteration 2

--- practice_independent_challenges/week5/reunion/lib/activity.rb
+++ practice_independent_challenges/week5/reunion/lib/activity.rb
@@ -1,18 +1,22 @@
 class Activity
   attr_reader :name,
               :participants
+              # :total_cost
               
   def initialize(name)
     @name = name
     @participants = {}
+    # @total_cost = 0
   end
 
   def add_participant(name, cost)
     @participants[name] = cost
+    # @total_cost = @total_cost + cost
     return @participants
   end
 
   def total_cost
    total_cost = @participants.values.sum
+   total_cost
   end
 end
--- practice_independent_challenges/week5/reunion/spec/activity_spec.rb
+++ practice_independent_challenges/week5/reunion/spec/activity_spec.rb
@@ -36,4 +36,16 @@ RSpec.describe do
       expect(@activity.total_cost).to eq(60)
     end
   end
+
+  describe 'ways to parse costs' do
+    it 'assess costs and balance' do
+      @activity.add_participant("Maria", 20)
+      @activity.add_participant("Luther", 40)
+
+      expect(@activity.total_cost).to eq(60)
+      expect(@activity.split).to eq(30)
+      expect(@activity.owed).to eq({"Maria" => 10, "Luther" => -10})
+
+    end
+  end
 end
